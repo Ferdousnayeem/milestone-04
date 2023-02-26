76c948091791b7f55a0b8164077f3684c98e97b7
Ferdousnayeem <mostafanayeem11@gmail.com>
2023-02-27 02:03:43 +0600

    javascript has just started with this milestone

---
+++ Practice Problems/operation.js
@@ -0,0 +1,16 @@
+/* var onionPrice = 20;
+var eggPrice = 10;
+
+
+console.log(onionPrice); */
+
+
+var num1 = 0.1;
+var num2 = 0.2;
+
+var sum = num1 + num2;
+    sum = sum.toFixed(2);
+    sum = parseFloat(sum); অথবা,
+    sum = parseInt(sum);
+
+console.log(sum);
