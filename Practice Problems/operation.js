ff995f45922852613f4422f2015ab20d92cd26b0
Ferdousnayeem <mostafanayeem11@gmail.com>
2023-02-27 15:17:00 +0600

    directory updated

--- Practice Problems/operation.js
+++ Practice Problems/operation.js
@@ -5,7 +5,7 @@ var eggPrice = 10;
 console.log(onionPrice); */
 
 
-var num1 = 0.1;
+/* var num1 = 0.1;
 var num2 = 0.2;
 
 var sum = num1 + num2;
@@ -13,4 +13,20 @@ var sum = num1 + num2;
     sum = parseFloat(sum); অথবা,
     sum = parseInt(sum);
 
-console.log(sum);
+console.log(sum); */
+
+var price = 33
+var name - Shabana
+var boxName = ‘Cocola;
+var 88_price = 34;
+var enum = -1;
+var _$box’78 = ‘Monika’; 
+var home-address = “kochu khet”;
+
+console.log(price);
+console.log(name);
+console.log(boxName);
+console.log(88_price);
+console.log(enum);
+console.log(_$box’78);
+console.log(home-address);
